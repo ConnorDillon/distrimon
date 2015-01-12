5f0643bca3d9177774ccaceb457bc913ea24df68
Connor Dillon <connor@cdillon.nl>
2015-01-13 00:20:30 +0100

    added Shell

--- src/main/scala/distrimon/Minion.scala
+++ src/main/scala/distrimon/Minion.scala
@@ -2,6 +2,7 @@ package distrimon
 
 class Minion(id: Int) extends BaseActor {
   val master = addChild[MasterTunnel]("master", id)
+  val shell = addActor[Shell]("shell")
   val pinger = addChild[Pinger]("pinger", master)
 
   def receive = unexpected
---
+++ src/main/scala/distrimon/Shell.scala
@@ -0,0 +1,33 @@
+package distrimon
+
+import akka.actor.ActorRef
+import scala.concurrent.Future
+import sys.process.stringSeqToProcess
+import Shell._
+
+class Shell extends BaseActor {
+  import context.dispatcher
+
+  def exec(sender: ActorRef, env: Envolope): Completed = env.msg match {
+    case Command(cmd) =>
+      log.info(s"executing command: $cmd")
+      val result = Result(stringSeqToProcess(Seq("sh", "-c", cmd)).!!.init)
+      log.info(s"command: ( $cmd ) completed with result: ${result.result}")
+      Completed(sender, env.reply(result))
+  }
+  
+  def receive = {
+    case e: Envolope => Future {
+      exec(sender, e)
+    } onSuccess {
+      case Completed(sender, env) => sender ! env
+    }
+  }
+
+  case class Completed(sender: ActorRef, envolope: Envolope)
+}
+
+object Shell {
+  case class Command(cmd: String)
+  case class Result(result: String)
+}
