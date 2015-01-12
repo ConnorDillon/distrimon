a01182ad418accd9236c22f60ece139db99c843a
Connor Dillon <connor@cdillon.nl>
2015-01-13 00:28:33 +0100

    renamed link to tunnel

--- src/main/scala/distrimon/RemoteActor.scala
+++ src/main/scala/distrimon/RemoteActor.scala
@@ -2,7 +2,7 @@ package distrimon
 
 import akka.actor.{ActorPath, ActorRef}
 
-class RemoteActor(dst: String, link: ActorRef) {
-  def tell(msg: Any)(implicit src: ActorPath): Unit = link ! Envolope(src.toStringWithoutAddress, dst, msg)
+class RemoteActor(dst: String, tunnel: ActorRef) {
+  def tell(msg: Any)(implicit src: ActorPath): Unit = tunnel ! Envolope(src.toStringWithoutAddress, dst, msg)
   def !(msg: Any)(implicit src: ActorPath) = tell(msg)
 }
