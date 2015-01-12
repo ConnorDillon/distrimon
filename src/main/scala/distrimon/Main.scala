5fed8c36d583aafbcd6213067cccee6f3b67a270
Connor Dillon <connor@cdillon.nl>
2015-01-13 00:21:12 +0100

    moved ping test to separate function and added cmd test

--- src/main/scala/distrimon/Main.scala
+++ src/main/scala/distrimon/Main.scala
@@ -1,18 +1,35 @@
 package distrimon
 
 import akka.actor.ActorSystem
+import akka.pattern.ask
+import akka.util.Timeout
+import concurrent.Await
+import concurrent.duration._
 
 object Main extends App {
   implicit val system = ActorSystem("distriSystem")
-  val minion0 = addActor[Minion]("minion0", 0)
-  Thread.sleep(5000)
-  val master = addActor[Master]("master", firstMaster)
-  Thread.sleep(20000)
-  system stop master
-  Thread.sleep(5000)
-  val newmaster = addActor[Master]("master", backupMaster)
-  Thread.sleep(20000)
-  system stop minion0
-  Thread.sleep(5000)
-  system stop newmaster
+
+  cmd()
+
+  def cmd(): Unit = {
+    import Shell._
+    implicit val timeout = Timeout(10.seconds)
+    val shell = addActor[Shell]("shell")
+    val future = shell ? Envolope("", "", Command("sleep 2; ls -la | wc -l"))
+    println(Await.result(future, 10.seconds))
+  }
+
+  def ping(): Unit = {
+    val minion0 = addActor[Minion]("minion0", 0)
+    Thread.sleep(5000)
+    val master = addActor[Master]("master", firstMaster)
+    Thread.sleep(20000)
+    system stop master
+    Thread.sleep(5000)
+    val newmaster = addActor[Master]("master", backupMaster)
+    Thread.sleep(20000)
+    system stop minion0
+    Thread.sleep(5000)
+    system stop newmaster
+  }
 }
